6d4f763360f58399270197c4ceacbe2d8f47519d
ZainAalabidin <zynly9303@gmail.com>
2024-10-01 14:09:34 +0000

    ok

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,6 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (name) => {
   process.stdout.write(`Your name is: ${name}`);
-
-  process.exit();
 });
 
 process.stdin.on('end', () => {
